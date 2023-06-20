153492cadcb8873b13b794d370ba7b2b31b220e7
Nick Stanovic <nickstanovic@gmail.com>
2023-06-20 19:16:44 -0400

    add context

---
+++ src/context/index.js
@@ -0,0 +1,17 @@
+import React, {useState, createContext} from "react"
+
+export const Context = createContext()
+
+export const ContextProvider = ({children}) => {
+  const [username, setUsername] = useState("")
+  const [secret, setSecret] = useState("")
+
+  const value = {
+    username,
+    setUsername,
+    secret,
+    setSecret
+  }
+
+  return <Context.Provider value={value}>{children}</Context.Provider>
+}
