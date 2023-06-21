d28ee64a48441450f2ea7b31c4320a28a68cfb54
Nick Stanovic <nickstanovic@gmail.com>
2023-06-21 01:17:55 -0400

    convert context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,16 +1,27 @@
-import React, {useState, createContext} from "react"
+import React, { ReactNode, useState, createContext } from "react"
 
-export const Context = createContext()
+interface IContext {
+  username: string,
+  setUsername: (username: string) => void,
+  secret: string,
+  setSecret: (secret: string) => void,
+}
 
-export const ContextProvider = ({children}) => {
-  const [username, setUsername] = useState("")
-  const [secret, setSecret] = useState("")
+export const Context = createContext<IContext>({} as IContext)
+
+interface ContextProviderProps {
+  children: ReactNode;
+}
+
+export const ContextProvider: React.FC<ContextProviderProps> = ({children}) => {
+  const [username, setUsername] = useState<string>("")
+  const [secret, setSecret] = useState<string>("")
 
   const value = {
     username,
     setUsername,
     secret,
-    setSecret
+    setSecret,
   }
 
   return <Context.Provider value={value}>{children}</Context.Provider>
